140ba3f1e95406ad2e2c45257ba569197102a4ff
Ivan <e4drcf@gmail.com>
2022-05-24 05:18:01 +0300

    url shortener from yaml

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/e4drcf/urlshort
+
+go 1.18
+
+require gopkg.in/yaml.v3 v3.0.0
---
+++ main/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/e4drcf/urlshort"
+	"net/http"
+)
+
+func main() {
+	mux := defaultMux()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github/e4drcf/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	err = http.ListenAndServe(":8080", yamlHandler)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintln(w, "Hello, world!")
+	if err != nil {
+		panic(err)
+	}
+}
